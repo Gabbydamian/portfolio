00af2cd74a9fa15c71aebba72696a1d0e3d3b16d
Gabriel Damian <gabbydamian92@gmail.com>
2022-07-11 17:07:00 +0100

    second commit. created About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,27 @@
 import './About.css';
 const ABOUT = () => {
   return(
-    
+    <div className="container">
+        <section id="about-section">
+            <div className="about-text">
+                <h2 className="about-text_heading">Hey! I'm <span>Damian Gabriel</span>
+                </h2>
+                <p className="about-text_body">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
+                id doloremque ab nostrum natus corporis aliquid, fugiat velit
+                voluptatum, at quae aut, eum maiores earum maxime. Doloribus
+                neque dolorem debitis!
+                </p>
+                <div className="buttons">
+                <a href="#resume" id="btn2" className="btn">Download Resume</a>
+                <a href="#contact" className="btn">Hire Me</a>
+                </div>
+            </div>
+            <div className="about-img">
+            
+            </div>
+        </section>
+    </div>
     )
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,14 @@ const NAVBAR = () => {
   return(
     <div className="navbar">
         <div className="logo-box">
-            <h1 className="logo">Astrid</h1>
+            <a href="#top"><h1 className="logo">Astrid</h1></a>
         </div>
         <nav>
             <ul className="nav-list">
                 <li className="nav-item active"><a href="#home" className="nav-link">Home</a></li>
                 <li className="nav-item"><a href="#skills"  className="nav-link">Skills</a></li>
                 <li className="nav-item"><a href="#projects"  className="nav-link">Projects</a></li>
+                <li className="nav-item"><a href="#resume"  className="nav-link">Resume</a></li>
                 <a className="btn" href="#contact">Hire me</a>
             </ul>
         </nav>
